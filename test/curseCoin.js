3ffe804326e2b9637e6695d802c4b9ba6329493a
Natalie Ventura Villasana <nat@storj.io>
2018-09-04 16:27:45 -0400

    adds other test

--- test/curseCoin.js
+++ test/curseCoin.js
@@ -12,12 +12,48 @@ contract("CurseCoin", function (accounts) {
       victim = accounts[1];
 
       return ccInstance.curse(victim, {
-        from: curser, value: web3.toWei('2', 'ether')
+        from: curser, value: web3.toWei("2", "ether")
       }).then(function (receipt) {
         return ccInstance.unfortunates(victim);
       }).then(function (cursed) {
         assert(cursed, "the victim was marked as cursed");
         return ccInstance.unfortunates(accounts[0]);
+      });
+    });
+  });
+
+  // it("allows an accursed account to nullify their own curse", function () {
+  //   return CurseCoin.deployed().then(function (instance) {
+  //     ccInstance = instance;
+  //     curser = accounts[2];
+  //     victim = accounts[3];
+
+  //     return ccInstance.curse(victim, {
+  //       from: curser, value: web3.toWei("2", "ether")
+  //     }).then(function (receipt) {
+  //       return ccInstance.nullify({
+  //         from: victim, value: web3.toWei("2", "ether")
+  //       });
+  //     }).then(function (uncurse) {
+  //       assert(!ccInstance.unfortunates(victim), "the victim is uncursed")
+  //       return ccInstance.unfortunates(victim);
+  //     });
+  //   });
+  // });
+
+  it("allows an account to check if they're cursed", function () {
+    return CurseCoin.deployed().then(function (instance) {
+      ccInstance = instance;
+      curser = accounts[2];
+      victim = accounts[3];
+
+      return ccInstance.curse(victim, {
+        from: curser, value: web3.toWei("2", "ether")
+      }).then(function (receipt) {
+        var curseCheck = ccInstance.amICursed({
+          from: victim, value: web3.toWei("2", "ether")
+        });
+        assert(curseCheck, "the victim should be cursed")
       });
     });
   });
