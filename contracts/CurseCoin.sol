3708a13c93544f86dd34559126b79c246a656131
Natalie Ventura Villasana <nat@storj.io>
2018-09-04 15:17:06 -0400

    adds CurseCoin contract plus one test

--- contracts/CurseCoin.sol
+++ contracts/CurseCoin.sol
@@ -1,5 +1,62 @@
 pragma solidity ^0.4.2;
 
-contract CurseCoin {
-  
+// SafeMath helps prevent integer overflows
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+// Ownable helps streamline contract inheritance
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+// Initiates this address as the owner
+contract CurseCoin is Ownable {
+    // adds SafeMath methods to uint256 types
+    using SafeMath for uint256;
+
+    string public name;
+    string public symbol;
+    uint256 public curseCost;
+
+    // Events are small empty functions that can be called as a signal or log.
+    event Curse(address _curser, address _accursed);
+    event Nullify(address _blessed);
+
+    mapping(address => bool) public unfortunates;
+
+    constructor() public {
+        name = "CurseCoin";
+        symbol = "CC";
+        curseCost = 1 ether;
+    }
+
+    // curse allows msg.sender to curse a victim if they pay the curseCost
+    function curse(address _victim) public payable {
+        // checks that curser is sending correct amount for cursing
+        require((msg.value > curseCost), "You gotta pay the troll toll");
+
+        // first check if curse's address
+        require((unfortunates[_victim] == false), "Victim is already cursed");
+
+        unfortunates[_victim] = true;
+        emit Curse(msg.sender, _victim);
+    }
+
+    // nullify allows msg.sender to uncurse themself (if applicable) if they pay the curseCost
+    function nullify() public payable {
+        // checks that curser is sending correct amount for uncursing
+        require((msg.value > curseCost), "You gotta pay the uncursing troll toll");
+
+        // first check if curse's address
+        require((unfortunates[msg.sender] == true), "You're not even cursed 🤔");
+
+        unfortunates[msg.sender] = true;
+        emit Nullify(msg.sender);
+    }
+
+    // withdraw allows the brilliant contract writers to cash out
+    function withdraw() public onlyOwner {
+        msg.sender.transfer(address(this).balance);
+    }
+
+    // amICursed allows msg.sender to check if they're cursed
+    function amICursed() public view returns (bool) {
+        return unfortunates[msg.sender];
+    }
 }
--- test/curseCoin.js
+++ test/curseCoin.js
@@ -1,6 +1,24 @@
 var CurseCoin = artifacts.require("./CurseCoin.sol");
 
 contract("CurseCoin", function (accounts) {
-  var curseCoinInstance;
+  var ccInstance;
+  var curser;
+  var victim;
 
-})
+  it("allows a curser to curse a victim", function () {
+    return CurseCoin.deployed().then(function (instance) {
+      ccInstance = instance;
+      curser = accounts[0];
+      victim = accounts[1];
+
+      return ccInstance.curse(victim, {
+        from: curser, value: web3.toWei('2', 'ether')
+      }).then(function (receipt) {
+        return ccInstance.unfortunates(victim);
+      }).then(function (cursed) {
+        assert(cursed, "the victim was marked as cursed");
+        return ccInstance.unfortunates(accounts[0]);
+      });
+    });
+  });
+});
